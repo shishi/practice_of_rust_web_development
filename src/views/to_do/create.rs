7c0275bb08b0a5945d6bcb9e9af5a8786320c3cc
Shigenobu Nishikawa <shishi@srevo.net>
2021-10-10 04:38:13 +0900

    finish chapter 5

--- src/views/to_do/create.rs
+++ src/views/to_do/create.rs
@@ -1,16 +1,29 @@
-use serde_json::value::Value;
-use serde_json::Map;
-use actix_web::HttpRequest;
-use crate::to_do;
-use crate::state::read_file;
-use crate::processes::process_input;
+use super::utils::return_state;
+use crate::database::establish_connection;
+use crate::diesel;
+use crate::models::item::item::Item;
+use crate::models::item::new_item::NewItem;
+use crate::schema::to_do;
+use actix_web::{HttpRequest, Responder};
+use diesel::prelude::*;
 
-pub async fn create(req: HttpRequest) -> String {
-    let state: Map<String, Value> = read_file(String::from("./state.json"));
-    let title: String = req.match_info().get("title").unwrap().to_string();
-    let title_reference: String = title.clone();
-    let item = to_do::to_do_factory(&String::from("pending"), title).expect("create ");
+pub async fn create(req: HttpRequest) -> impl Responder {
+    let title = req.match_info().get("title").unwrap().to_string();
+    let title_ref = title.clone();
 
-    process_input(item, "create".to_string(), &state);
-    return format!("{} created", title_reference);
+    let connection = establish_connection();
+    let items = to_do::table
+        .filter(to_do::columns::title.eq(title_ref.as_str()))
+        .order(to_do::columns::id.asc())
+        .load::<Item>(&connection)
+        .unwrap();
+
+    if items.len() == 0 {
+        let new_post = NewItem::new(title);
+        let _ = diesel::insert_into(to_do::table)
+            .values(&new_post)
+            .execute(&connection);
+    }
+
+    return return_state();
 }
--- src/views/to_do/delete.rs
+++ src/views/to_do/delete.rs
@@ -1,21 +1,22 @@
 use super::utils::return_state;
+use crate::database::establish_connection;
+use crate::diesel;
 use crate::json_serialization::to_do_item::ToDoItem;
-use crate::processes::process_input;
-use crate::state::read_file;
-use crate::to_do::to_do_factory;
+use crate::models::item::item::Item;
+use crate::schema::to_do;
 use actix_web::{web, HttpResponse};
-use serde_json::value::Value;
-use serde_json::Map;
+use diesel::prelude::*;
 
 pub async fn delete(to_do_item: web::Json<ToDoItem>) -> HttpResponse {
-    let state = read_file(String::from("./state.json"));
-    let title = to_do_item.title.clone();
-    let status = to_do_item.status.clone();
+    let title_ref = to_do_item.title.clone();
+    let connection = establish_connection();
 
-    match to_do_factory(&status, title) {
-        Err(_) => return HttpResponse::BadRequest().json(format!("{} not accepted", status)),
-        Ok(item) => process_input(item, String::from("delete"), &state),
-    }
-
+    let items = to_do::table
+        .filter(to_do::columns::title.eq(title_ref.as_str()))
+        .order(to_do::columns::id.asc())
+        .load::<Item>(&connection)
+        .unwrap();
+    let _ = diesel::delete(&items[0]).execute(&connection);
+    
     return HttpResponse::Ok().json(return_state());
 }
